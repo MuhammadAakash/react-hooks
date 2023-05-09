1f63ec0b389348b76470cef506b99813ecb5ece7
Mohammad Aakash <muhammadaakash249@gmail.com>
2023-05-09 18:31:12 +0500

    use State Hook completed with the explanantion

---
+++ src/components/StateHook.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+const StateHook = () => {
+  const [counter, setCounter] = useState(0);
+  const [inputValue, setInputValue] = useState("");
+  const increment = () => {
+    setCounter(counter + 1);
+  };
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const decrement = () => {
+    if (counter > 0) {
+      setCounter(counter - 1);
+    }
+  };
+  return (
+    <>
+      <h1>State Hook</h1>
+      <div>{counter}</div>
+      <div>
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
+      </div>
+
+      <input type="text" placeholder="Enter anything" onChange={handleChange} />
+      {inputValue && <div>{inputValue}</div>}
+    </>
+  );
+};
+
+export default StateHook;
