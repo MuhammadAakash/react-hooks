94f1374bed089eca65c7f9a88d16e1ad1d7219a7
Mohammad Aakash <muhammadaakash249@gmail.com>
2023-05-11 13:17:02 +0500

    Added explanation and implementation of useMemo Hook

---
+++ src/components/useMemo/MemoHook.jsx
@@ -0,0 +1,51 @@
+import axios from "axios";
+import React, { useEffect, useMemo, useState } from "react";
+
+const MemoHook = () => {
+  const [usersData, setUsersData] = useState([]);
+  const [showHook, setShowHook] = useState(false);
+  const showComponent = () => {
+    setShowHook(!showHook);
+  };
+
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
+      setUsersData(res.data);
+    });
+  }, []);
+
+  const findLargestName = (names) => {
+    if (!names || names.length === 0) return undefined;
+    let longestName = "";
+    for (let i = 0; i < names.length; i++) {
+      if (names[i].name.length > longestName.length) {
+        longestName = names[i].name;
+      }
+    }
+    return longestName;
+  };
+
+  const getTheLongestName = useMemo(
+    () => findLargestName(usersData),
+    [usersData]
+  );
+
+  return (
+    <>
+      <div>
+        <button className="button" onClick={showComponent}>
+          Show useMemo Hook
+        </button>
+      </div>
+
+      {showHook && (
+        <div>
+          <h1>useMemo Hook</h1>
+          <p>{getTheLongestName}</p>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default MemoHook;
